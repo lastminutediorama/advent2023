7a67b09e77f117e65b969393363f6dde12b1eba9
lastminutediorama <markatsig@proton.me>
2023-12-04 01:53:56 -0600

    day03, part2, very ugly code :)

--- src/advent2023/day03.clj
+++ src/advent2023/day03.clj
@@ -2,7 +2,15 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str :refer [split-lines]]))
 
-(defn near-symbol? [r-above c-left r-below c-right arr2d]
+(def found-gears (atom []))
+
+(defn coords->digits [coords arr2d]
+  (Integer/parseInt
+   (clojure.string/join ""
+                        (mapv (fn [[r c]]
+                                (aget arr2d r c)) coords))))
+
+(defn near-symbol? [r-above c-left r-below c-right arr2d & n]
   (let [min-row (max r-above 0)
         max-row (min r-below (dec (alength arr2d)))
         min-col (max c-left 0)
@@ -13,25 +21,24 @@
     ; cycle through coords, and if any are a symbol, we collect it
     (let [symbols (filter (fn [[r c]]
                             (let [x (aget arr2d r c)]
+                              (when (= x \*) ;; For Part 2
+                                (swap! found-gears conj {:gearloc [r c]
+                                                         :num (coords->digits (first n) arr2d)}))
                               (not (or (Character/isDigit x) (= x \.)))))
                           coords)]
       (seq symbols))))
 
-(defn coords->digits [coords arr2d]
-  (Integer/parseInt (clojure.string/join "" (mapv (fn [[r c]]
-                                 (aget arr2d r c)) coords))))
 
 (defn find-parts
   "checks vec of coords (ncoll) for neighboring symbols"
   [ncoll arr2d]
   (reduce (fn [parts n]
-            (println "n: " n)
             (let [rowabove (dec (first (first n)))
                   rowbelow (inc (first (first n)))
                   colleft (dec (last (first n)))
-                  colright (inc (last (last n)))] 
+                  colright (inc (last (last n)))]
               (if (near-symbol? rowabove
-                                colleft rowbelow colright arr2d)
+                                colleft rowbelow colright arr2d n)
                 (conj parts (coords->digits n arr2d))
                 parts)))
           []
@@ -66,11 +73,34 @@
         lines (split-lines data-file)
         arr (mapv vec lines)
         ar2d (to-array-2d arr)
-        founddigs (find-nums arr)
-        ]
-     (apply + (find-parts founddigs ar2d))))
+        foundnums (find-nums arr)]
+    (apply + (find-parts foundnums ar2d))))
+
+(defn getratio [gcoord]
+  (let [matching (filter (fn [fg]
+                           (= gcoord (:gearloc fg))) @found-gears)]
+    (apply * (mapv :num matching))))
+
+(defn getratios []
+  (let [dupegears (mapv (fn [[g c]]
+                          g)
+                        (filter (fn [[g c]]
+                                  (> c 1))
+                                (frequencies (mapv :gearloc @found-gears))))]
+    (mapv getratio dupegears)))
+
+(defn day03-2 []
+  (reset! found-gears [])
+  (let [data-file (slurp (io/resource "day03.txt"))
+        lines (split-lines data-file)
+        arr (mapv vec lines)
+        ar2d (to-array-2d arr)
+        foundnums (find-nums arr)]
+    (find-parts foundnums ar2d))
+  (apply + (getratios)))
 
 (comment
   (day03-1)
-  
+
+  (day03-2)
   #_endc)
